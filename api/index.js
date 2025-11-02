efb081039a09556b9e51fad65b2200e854782998
thebobbbond <me@bobbond.co.uk>
2025-11-02 12:07:57 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
   try {
     const makeWebhookUrl = "https://hook.eu2.make.com/dm34sewci9xme214l9coxtsuuq0o9gyi";
+
     const response = await fetch(makeWebhookUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -10,7 +11,8 @@ export default async function handler(req, res) {
     const data = await response.text();
     res.status(200).send(data);
   } catch (error) {
-    console.error("Error calling Make webhook:", error);
+    console.error("Error forwarding to Make:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 }
+
